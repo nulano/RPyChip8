89fd59c79086f8a1e27428da22eedd899279db46
nulano <nulano@nulano.eu>
2021-01-23 04:00:38 +0100

    add keyboard, sound support to gui

--- gui/pygame/root.py
+++ gui/pygame/root.py
@@ -1,4 +1,6 @@
 import subprocess
+from array import array
+from math import pi, sin
 
 import pygame
 from PIL import Image, ImageEnhance
@@ -8,12 +10,25 @@ from emulator.error import errorstream
 from emulator.io.stdio import Stdio, StdioTest
 from emulator.types import uint64_t, uint8_t
 
-speed_factor = 1
+speed_factor = 10
 crt_factor = 0.7
+sound_fq = 440
+sound_amp = 0.1
+
+keymap = [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4,
+          pygame.K_q, pygame.K_w, pygame.K_e, pygame.K_r,
+          pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_f,
+          pygame.K_z, pygame.K_x, pygame.K_c, pygame.K_v]
 
 frames_per_s = 60
+ms_per_frame = 1000 / 60
 cycles_per_ms = 1000 * speed_factor
-cycles_per_frame = 1000 * cycles_per_ms / frames_per_s
+cycles_per_frame = cycles_per_ms * ms_per_frame
+
+keypad = [keymap[13], keymap[0], keymap[1], keymap[2],
+          keymap[4], keymap[5], keymap[6], keymap[8],
+          keymap[9], keymap[10], keymap[12], keymap[14],
+          keymap[3], keymap[7], keymap[11], keymap[15]]
 
 
 class Screen:
@@ -38,8 +53,8 @@ class Screen:
 
 class Io_Impl(Io):
     def __init__(self):
-        self.delay = uint8_t(0)
-        self.sound = uint8_t(0)
+        self.sound = create_sound()
+        self.delay = 0L
         self.last = 0L
         self.offset = 0L
 
@@ -57,21 +72,30 @@ class Io_Impl(Io):
             self.offset += ahead
 
     def is_key_down(self, key):
-        raise NotImplementedError
+        return pygame.key.get_pressed()[keypad[key]]
 
     def next_key(self):
-        raise NotImplementedError
+        print "waiting for key..."
+        for event in pygame.event.get(pygame.KEYDOWN):
+            pass
+        while True:
+            for event in pygame.event.get(pygame.KEYDOWN):
+                # print event
+                try:
+                    i = keypad.index(event.key)
+                except ValueError:
+                    pass
+                else:
+                    return uint8_t(i)
 
     def set_sound(self, delay):
-        # print "set sound", delay
-        self.sound = self.last + long(delay) * cycles_per_frame
+        self.sound.stop()
+        self.sound.play(loops=-1, maxtime=long(delay) * ms_per_frame)
 
     def set_delay(self, delay):
-        # print "set delay", delay
         self.delay = self.last + long(delay) * cycles_per_frame
 
     def get_delay(self):
-        # print "get delay"
         diff = self.delay - self.last
         if diff <= 0:
             return uint8_t(0)
@@ -79,7 +103,22 @@ class Io_Impl(Io):
         return uint8_t(min(diff, 255))
 
 
+def create_sound():
+    pygame.mixer.pre_init(channels=1)
+    fq, fmt, ch = pygame.mixer.get_init()
+
+    period = fq / sound_fq
+    amplitude = int((2 ** (abs(fmt) - 1) - 1) * sound_amp)
+
+    samples = [int(sin(pi * x / period) * amplitude) for x in xrange(period)]
+
+    return pygame.mixer.Sound(array("h", samples))
+
+
 def main(argv):
+    pygame.init()
+    window = pygame.display.set_mode((640, 320))
+
     screen = Screen()
 
     proc = subprocess.Popen(['./chip8-c'], executable='./chip8-c.exe',
@@ -87,9 +126,6 @@ def main(argv):
     pipe = Stdio(proc.stdout, proc.stdin)
     chip8 = Chip8_Rpc(pipe, Io_Impl())
     chip8.initialize(argv[1])
-
-    pygame.init()
-    window = pygame.display.set_mode((640, 320))
 
     running = True
     while running:
